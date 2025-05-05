de48596261ab0fd3297663fd7186a1fc96698546
Prakhar Agrawal <prakhar281005@gmail.com>
2025-05-05 15:27:02 -0700

    made remote control work

--- remote_control_motor.py
+++ remote_control_motor.py
@@ -5,10 +5,10 @@ import math
 # ── Motor driver pins (Cytron Maker Drive) ─────────────────────────────────────
 SPEED = 1023                           # full PWM (0-1023 for ESP32)
 
-M1A = machine.PWM(machine.Pin(13))
-M1B = machine.PWM(machine.Pin(12))
-M2A = machine.PWM(machine.Pin(27))
-M2B = machine.PWM(machine.Pin(33))
+M1A = machine.PWM(machine.Pin(12))
+M1B = machine.PWM(machine.Pin(13))
+M2A = machine.PWM(machine.Pin(33))
+M2B = machine.PWM(machine.Pin(27))
 
 def m1_fwd():   M1A.duty(SPEED); M1B.duty(0)
 def m1_rev():   M1A.duty(0);     M1B.duty(SPEED)
@@ -62,14 +62,29 @@ while True:
         print("mid %.1f cm  left %.1f cm  right %.1f cm"
               % (d_mid, d_left, d_right))
         
+        
+            
+        if d_left < 20 and d_right < 20 and d_mid < 20:
+            all_stop()
+            continue
+            
         if d_left < 5:
             m1_fwd()
+            
+            
+        elif d_left >=5:
+            m1_stop()
+            
            
         if d_right < 5:
             m2_fwd()
             
-        if d_left < 5 and d_right < 5 and d_mid < 5:
-            all_stop()
+        elif d_left >=5:
+            m2_stop()
+            
+        if d_mid <=5:
+            m1_rev()
+            m2_rev()
             
         
 
